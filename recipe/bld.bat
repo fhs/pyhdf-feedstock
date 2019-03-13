b3c2b20442ee1239316321d8df030e561ef4fa6b
Fazlul Shahriar <fshahriar@gmail.com>
2019-03-12 23:49:12 -0400

    try to cross-compile

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,5 +5,7 @@ set hdf4=
 set LIBRARY_DIRS=%LIBRARY_BIN%;%LIBRARY_LIB%
 set INCLUDE_DIRS=%LIBRARY_INC%
 
+set DISTUTILS_USE_SDK=
+
 REM %PYTHON% -m pip install . --no-deps -vv
 %PYTHON% -m pip install --no-deps --ignore-installed --no-cache-dir -vvv .
